b909e5e931668eade171c4e672f9ea3140d9da76
vkreing18 <mirzaasktitan@gmail.com>
2024-10-07 01:04:53 +0530

    Hello

---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    return res.json({ message: 'This is Inside Container' });
+});
+
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));

883404b69f1584586da0c57a55994da76a01c8b5
vkreing18 <mirzaasktitan@gmail.com>
2024-10-07 02:15:06 +0530

    Docker compose File Added

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    return res.json({ message: 'This is Inside Container' });
+    return res.json({ message: 'This is Inside Container New Entry Point' });
 });
 
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
